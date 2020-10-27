726abf55d42c90ce6766d47f86738f927b67e763
VSZM <wayasam@gmail.com>
2020-10-28 00:27:18 +0100

    Fixing typo in test

--- tests/integration/test_training.py
+++ tests/integration/test_training.py
@@ -5,4 +5,4 @@ def test_run_training():
     import training_model
     assert os.path.exists('models')
     assert os.path.exists('models/' + config.model_to_use)
-    assert os.path.exists('models/' + config.pipe)
+    assert os.path.exists('models/' + config.pipe_to_use)
